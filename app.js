84de26fb6676c6ae64a1cbe52624069d0dde4dae
Gyulo94 <qkrcjfrb112@gmail.com>
2023-12-27 22:19:38 +0900

    페이지 스타일링

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ app.use(express.static('views'))
 app.use(shopRoutes);
 app.use('/admin', adminRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, '/views', '404.html'))
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
 app.listen(PORT, () => {
