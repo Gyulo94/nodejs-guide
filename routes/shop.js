a510665b7576cc6933e0ede83efa048ec4f6fdfe
Gyulo94 <qkrcjfrb112@gmail.com>
2023-12-27 23:49:47 +0900

    EJS로 작업하기

--- routes/shop.js
+++ routes/shop.js
@@ -5,8 +5,17 @@ const adminData = require('./admin')
 const router = express.Router();
 
 router.get('/',(req, res) => {
-    console.log(adminData.products);
-    res.sendFile(path.join(rootDir, 'views', 'shop.html')); 
+    const products = adminData.products;
+    res.render('shop', {
+        prods: products, 
+        pageTitle: 'Shop',
+        path:'/',
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true
+    })
+    // console.log(adminData.products);
+    // res.sendFile(path.join(rootDir, 'views', 'shop.html')); 
 })
 
 module.exports = router;
