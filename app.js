fc8ebfc3faefad7d93f7510d744725b87ee32bbf
Gyulo94 <qkrcjfrb112@gmail.com>
2023-12-27 17:04:00 +0900

    Express 라우터 사용

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
+
+const app = express();
+
 const PORT = 8080;
+
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/add-product',(req, res) => {
-    console.log('in another middleware!');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
-})
-
-app.post('/product', (req, res) => {
-    console.log(req.body);
-    res.redirect('/');
-})
-
-app.get('/',(req, res) => {
-    console.log('in another middleware!');
-    res.send('<h1>Hello from Express!</h1>')
-})
+app.use(shopRoutes);
+app.use(adminRoutes);
 
 app.listen(PORT, () => {
     console.log(`서버 가동 중...${PORT}`);
