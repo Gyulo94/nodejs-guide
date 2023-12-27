ddb56f9281e5ab907b7fdf086a74d2ad53e99055
Gyulo94 <qkrcjfrb112@gmail.com>
2023-12-27 21:58:40 +0900

    404 페이지 반환

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
+const path = require('path')
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 
-const PORT = 8080;
+const PORT = 8090;
 
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({extended: false}));
-
+app.use(express.static('views'))
 app.use(shopRoutes);
 app.use('/admin', adminRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>404 Page not found</h1>')
+    res.status(404).sendFile(path.join(__dirname, '/views', '404.html'))
 })
 
 app.listen(PORT, () => {
