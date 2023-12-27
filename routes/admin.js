3b43629a971cd9d682af3d99f2006e7a21ce9487
Gyulo94 <qkrcjfrb112@gmail.com>
2023-12-27 23:00:00 +0900

    요청 및 사용자 간의 데이터 공유

--- routes/admin.js
+++ routes/admin.js
@@ -5,6 +5,8 @@ const rootDir = require('../util/path')
 
 const router = express.Router();
 
+const products = [];
+
 // * /admin/add-product => GET
 router.get('/add-product',(req, res) => {
     res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
@@ -12,8 +14,9 @@ router.get('/add-product',(req, res) => {
 
 // * /admin/add-product => POST
 router.post('/add-product', (req, res) => {
-    console.log(req.body);
+    products.push({ title:req.body.title });
     res.redirect('/');
 })
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
